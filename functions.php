9a8afb35ed66605f42ddb6cb83483a3093ab8674
Leo Gopal <leo@leogopal.com>
2017-05-09 21:57:38 +0200

    Verbose docblocks for clarity

--- functions.php
+++ functions.php
@@ -1,10 +1,16 @@
 <?php
+// If accessed directly, exit.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'wp_enqueue_scripts', 'sera_enqueue_styles' );
-function sera_enqueue_styles() {
+/**
+ * Enqueues (always have to check that spelling)
+ * all the relevant theme styles and scripts
+ * and versions them based on the last modified time
+ * we do this for cache-busting purposes.
+ */
+function sera_frontend_enqueues() {
 	$parent_style = 'divi-style'; // This is the 'parent-style'.
 	$parent_stylesheet = get_template_directory_uri() . '/style.css';
 	$child_script = get_stylesheet_directory_uri() . '/assets/js/scripts.js';
@@ -30,13 +36,26 @@ function sera_enqueue_styles() {
 		true
 	);
 }
+add_action( 'wp_enqueue_scripts', 'sera_frontend_enqueues' );
 
-// Include the Divi modules used in this project as drop-ins.
+/**
+ * Includes the file for custom modules
+ * to behave as theme drop-ins not plugins
+ */
 include get_stylesheet_directory() . '/modules/init.php';
 
-function my_et_builder_post_types( $post_types ) {
+/**
+ * Enables the divi builder on specific CPTs
+ * in this case it will be 'product'
+ * by adding it to the $post_types array filter.
+ *
+ * @param $post_types
+ *
+ * @return array
+ */
+function sera_et_builder_post_types( $post_types ) {
 	$post_types[] = 'product';
 	return $post_types;
 }
-add_filter( 'et_builder_post_types', 'my_et_builder_post_types' );
+add_filter( 'et_builder_post_types', 'sera_et_builder_post_types' );
 
--- modules/init.php
+++ modules/init.php
@@ -1,9 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: leogopal
- * Date: 5/8/17
- * Time: 6:51 PM
- */
 
+/**
+ * Includes the image intense plugin as theme drop-in.
+ * Version latest as of: 2017-05-09
+ * @see https://besuperfly.com/product/image-intense-plugin/
+ */
 include 'image-intense/image-intense.php';
