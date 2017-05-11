ce2e173431a39275c86044fe6b1164faecb85940
Leo Gopal <leo@leogopal.com>
2017-05-11 02:24:41 +0200

    Tabbing functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function ($) {
-  // Code Here
+
+    // Tab functionality for Woo Content Area.
+    $('.sera-tabbed-container .tab-links li').click(function (event) {
+
+        $('ul.tab-links li').removeClass('active');
+        $('.tabbed').removeClass('active');
+
+        var tabID = $(this).attr('data-tab');
+
+        $(this).addClass('active');
+        $('#' + tabID).addClass('active');
+
+    });
 });
